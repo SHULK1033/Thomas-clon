5fa1ff7a850a18137d3b7fe941bbc03d6de05738
Andres Arias <andres10333@hotmail.com>
2020-11-05 15:46:15 -0500

    ajustes y trampolin funcionando

--- Assets/my_scripts/Trampoline.cs
+++ Assets/my_scripts/Trampoline.cs
@@ -2,17 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trampoline : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Trampoline : MonoBehaviour {
+    public float jumpForce = 90f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnCollisionEnter2D (Collision2D other) {
+        if (other.gameObject.tag == "Player" && other.gameObject.name != "Cubo4") {
+            other.gameObject.GetComponent<Rigidbody2D> ().velocity = (Vector2.up * jumpForce);
+        }
     }
 }
